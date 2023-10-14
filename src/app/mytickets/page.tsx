10e545ab2e6f03cee3f8e0d5fd573b126dcbdf41
ardimr <rizkyardimaulana@gmail.com>
2023-10-14 21:54:30 +0700

    Initialize mytickets page

---
+++ src/app/mytickets/page.tsx
@@ -0,0 +1,12 @@
+import RootLayout from '@/components/Layout/Layout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <RootLayout>
+      <div>This is My Ticket Page</div>
+    </RootLayout>
+  )
+}
+
+export default page
