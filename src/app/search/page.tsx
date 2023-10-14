b1e778803ddf7666a112f2372171e5771a662c64
ardimr <rizkyardimaulana@gmail.com>
2023-10-14 21:54:44 +0700

    Added Suspense on loading

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { Suspense } from 'react'
 import SearchTickets from '@/components/SearchTickets/SearchTickets'
 import TravelCard from '@/components/TravelCard/TravelCard'
 import { QueryClientProvider, QueryClient } from 'react-query'
@@ -12,8 +12,11 @@ const Search = () => {
   return (
     <RootLayout>
       <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
-          <QueryClientProvider client={queryClient}> 
-            <SearchTickets/>
+          <QueryClientProvider client={queryClient}>
+            <div style={{marginTop:"20px"}}/>
+            <Suspense fallback={<div>Loading Search Form</div>} >
+              <SearchTickets/>
+            </Suspense>
           </QueryClientProvider>
       </div>
     </RootLayout>
