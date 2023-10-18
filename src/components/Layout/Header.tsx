29824c6f56cdb5502ecdc35db5da3395d55ef553
ardimr <rizkyardimaulana@gmail.com>
2023-10-18 23:13:41 +0700

    Modified Header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -60,15 +60,32 @@ const NavBar = () => {
                     <NavItem link='/profile' label='Profile' />
                   </li>
                   <li>
-                    <SecondaryButton style={{padding:"5px 10px", marginLeft:"5px"}} onClick={handleLogout}>Logout</SecondaryButton>
+                    <SecondaryButton style={{padding:"5px 10px", marginLeft:"5px", backgroundColor:"white", color:"#4899e5"}} onClick={handleLogout}>Logout</SecondaryButton>
                   </li>
                 </>
               : <>
                 <li style={{marginRight:"10px"}}>
-                  <SecondaryButton style={{padding:"5px 10px", marginLeft:"5px"}} onClick={() => { router.push('/login') }}>Login</SecondaryButton>
+                  <SecondaryButton style={{padding:"5px 10px", marginLeft:"5px", backgroundColor:"white", color:"#4899e5"}} onClick={() => { router.push('/login') }}>Login</SecondaryButton>
+                  {/* <button 
+                    style={{
+                      padding:"5px 10px",
+                      marginLeft:"5px",
+                      backgroundColor:"white",
+                      color:"blue",
+                      // outline:"1px solid white",
+                      boxSizing:"border-box",
+                      borderRadius:"5px",
+                      border: 0,
+                      alignSelf:"center",
+                      justifySelf:"center"
+                    }}
+                    onClick={() => { router.push('/login') }}
+                  >
+                    Login
+                  </button> */}
                 </li>
                 <li style={{marginRight:"10px"}}>
-                  <SecondaryButton style={{padding:"5px 10px"}} onClick={() => { router.push('/registration') }}>Register</SecondaryButton>
+                  <SecondaryButton style={{padding:"5px 10px", marginLeft:"5px", color:"white", border:"1px solid white"}} onClick={() => { router.push('/registration') }}>Register</SecondaryButton>
                 </li>
               </>
           }
@@ -82,17 +99,17 @@ const NavBar = () => {
 
 const Header = () => {
   return (
-    <header style={{ position: "sticky", top: "0", backgroundColor: "white" }}>
+    <header style={{ position: "sticky", top: "0", backgroundColor: "#43A1FE" }}>
       <div className={styles['container']}>
         <div style={{ display: "flex", justifySelf: "center", padding: "10px", alignItems:"center" }}>
-          <LiaTrainSolid size={48}/>
-          <div style={{display:"flex", fontWeight:"bold", fontSize:"24px", width: "300px"}}>
+          {/* <LiaTrainSolid size={48}/> */}
+          <div style={{display:"flex", fontWeight:"bold", fontSize:"24px", width: "300px", color:"white", fontFamily:"Montserrat, sans-serif"}}>
             Train Booking System
           </div>
         </div>
         <NavBar />
       </div>
-      <div style={{ width: "100%", height: "5px", background: "lightgray" }} />
+      {/* <div style={{ width: "100%", height: "5px", background: "lightgray" }} /> */}
 
     </header>
   )
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import Header from './Header'
 import styles from './styles.module.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query'
+import { AxiosError, isAxiosError } from 'axios'
+import { error } from 'console'
+
+const handleError = (error:unknown) => {
+  if (!isAxiosError(error)) {
+    return "Unknown error";
+  }
+  if (error instanceof Error && 'response' in error) {
+    const response = error.response!
+    if (response.status === 401) {
+      console.error('Unauthorized error:', error);
+      // Handle unauthorized error, e.g., redirect to a login page
+      window.location.href = '/login';
+    } else if (response.status === 404) {
+      console.error('Not found error:', error);
+      // Handle not found error, e.g., display an error message
+      alert('The requested resource was not found.');
+    } else {
+      console.error('An error occurred:', error);
+      // Handle other errors, e.g., display a generic error message
+    }
+  } else {
+    console.error('An error occurred:', error);
+  }
+};
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: handleError
+  })
+
+})
 
 const RootLayout = ({
   children,
 }: {
   children: React.ReactNode
 }) => {
-  const queryClient = new QueryClient()
   return (
     <div className={styles['layout']}>
       <QueryClientProvider client={queryClient}>
-        <Header/>
+        <Header />
         <main> {children} </main>
       </QueryClientProvider>
     </div>
